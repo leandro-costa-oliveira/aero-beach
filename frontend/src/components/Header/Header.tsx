78a704f1b6d13e0df964bf94e98a94f3d2977ba8
caio_rdc <caio.crdc@gmail.com>
2025-09-04 16:36:36 -0300

    refactor:  mudança de nome de arquivo e estilo de links

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,27 +1,28 @@
 import logo from "../../assets/img/aero-logo.png";
 import { Nav, Navbar } from "react-bootstrap";
 import styles from "./Header.module.css";
+import index from "../../index.module.css"
 
 export function Header() {
   return (
     <Navbar expand="sm" className="bg-dark navbar-dark container-fluid px-2">
-      <Navbar.Brand href="#home" className={`${styles.logoContainer} gap-1 px-4 rounded-5 d-flex`}>
+      <Navbar.Brand href="#inicio" className={`${styles.logoContainer} gap-1 px-4 rounded-5 d-flex`}>
         <img src={logo} className={styles.img} />
         AeroBeach
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav"/>
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav>
-          <Nav.Link href="#inicio">
+          <Nav.Link href="#inicio" className={index.lightLink}>
             Inicio
           </Nav.Link>
-          <Nav.Link href="#torneios">
+          <Nav.Link href="#torneios" className={index.lightLink}>
             Torneios
           </Nav.Link>
-          <Nav.Link href="#rankings">
+          <Nav.Link href="#rankings" className={index.lightLink}>
             Rankings
           </Nav.Link>
-          <Nav.Link href="#sobre">
+          <Nav.Link href="#sobre" className={index.lightLink}>
             Sobre
           </Nav.Link>
         </Nav>
