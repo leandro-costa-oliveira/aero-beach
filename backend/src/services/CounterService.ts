e0e296cfd12e695dbb03ef69e7c3f21f92c1e7a2
caio_rc <caio.crdc@gmail.com>
2025-09-07 14:38:23 -0300

    conexão da função doCount com o db

--- backend/src/services/CounterService.ts
+++ backend/src/services/CounterService.ts
@@ -7,7 +7,24 @@ export class CounterService {
     return prisma.contador.findFirst();
   }
 
+  //cria uma linha na tabela
   async doCount() {
-    return this.count++;
+    const contador = await prisma.contador.findFirst();
+    if (contador === null) {
+      const nContador = await prisma.contador.create({ data: { contador: 1 } });
+      return nContador.contador;
+    }
+
+    const novoNumero = contador.contador + 1;
+    const contadroAtualizado = await prisma.contador.update({
+      where: {
+        id: contador.id,
+      },
+      data: {
+        contador: novoNumero,
+      },
+    });
+    return contadroAtualizado.contador;
   }
 }
+//incrementar +1 ao contador a cada refresh
