24c5adc4ba7da93bf7de9f38c4559892b88164d1
caio_rc <caio.crdc@gmail.com>
2025-09-07 13:26:43 -0300

    corrigindo importação do prisma

--- backend/src/services/DatabaseService.ts
+++ backend/src/services/DatabaseService.ts
@@ -1,7 +1,10 @@
 import { PrismaBetterSQLite3 } from "@prisma/adapter-better-sqlite3";
-import { PrismaClient } from "@prisma/client/extension";
+import { PrismaClient } from "../../generated/prisma";
+
+
+
 
 const adapter = new PrismaBetterSQLite3({
   url: "file:./prisma/aerobeach.db",
 });
-export const prisma = new PrismaClient({ adapter });
+export const prisma = new PrismaClient();
