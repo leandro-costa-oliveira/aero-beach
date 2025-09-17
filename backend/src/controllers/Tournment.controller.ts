cc515e7b73e11587e1dd817c5b1ef949689bc47f
vittor Javidan <vittor.javidan.t@gmail.com>
2025-09-17 11:35:48 -0300

    feat: get /torneio/latest implementation

--- backend/src/controllers/Tournment.controller.ts
+++ backend/src/controllers/Tournment.controller.ts
@@ -1,9 +1,16 @@
 import { Get, JsonController } from "routing-controllers";
+import { TournmentService } from "../services/TournmentService";
 
 @JsonController("/torneios")
 export class TournamentController {
+  private tournmentService: TournmentService
+  constructor() {
+    this.tournmentService = new TournmentService();
+  }
 
   @Get("/latest")
   async lastTournament() {
+    const lastTournament = await this.tournmentService.lastTournament();
+    return { tournament: lastTournament || null };
   }
 }
