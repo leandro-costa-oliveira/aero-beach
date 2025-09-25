52552b25295fa0afa96197ad670eaac6d1b7d0de
caio_rc <caio.crdc@gmail.com>
2025-09-25 07:41:20 -0300

    feat: criação do card de torneios

--- frontend/src/components/CardTorneios/CardTorneios.tsx
+++ frontend/src/components/CardTorneios/CardTorneios.tsx
@@ -1,24 +1,40 @@
-import { Card, Col, ListGroup, Row } from "react-bootstrap";
+import { Button, Card, Col, ListGroup, Row } from "react-bootstrap";
 
-export function CardTorneios() {
+type TorneioProps = {
+  id: string;
+  nome: string;
+  federado?: boolean;
+  realizadoEm: string;
+  limiteInscricao: string;
+  preco: number;
+};
+
+export function CardTorneios({
+  nome,
+  federado,
+  realizadoEm,
+  limiteInscricao,
+  preco,
+}: TorneioProps) {
   return (
-    <Card className="border">
+    <Card className="border-dark">
       <Card.Header className="bg-dark text-light">
-        <Row className="">
-          <Col>#nomeDoTorneio</Col>
-          <Col className="d-flex justify-content-end">#Federado?</Col>
+        <Row>
+          <Col>{nome}</Col>
+          <Col className="d-flex justify-content-end">
+            {federado ? "Federado" : "Não Federado"}
+          </Col>
         </Row>
       </Card.Header>
       <Card.Body>
-        <ListGroup variant="flush">
-          <ListGroup.Item>#</ListGroup.Item>
-          <ListGroup.Item>#</ListGroup.Item>
-          <ListGroup.Item>#</ListGroup.Item>
+        <div className="d-flex justify-content-end text-secondary">
+          {new Date(realizadoEm).toLocaleDateString()}
+        </div>
+        <ListGroup className="mt-2 mb-4">
+          <ListGroup.Item className="border-secondary">Inscrições até: {new Date(limiteInscricao).toLocaleDateString()}</ListGroup.Item>
+          <ListGroup.Item className="border-secondary">Valor do incrição {preco}</ListGroup.Item>
         </ListGroup>
-        <Row>
-          <Col></Col>
-          <Col>#data</Col>
-        </Row>
+            <Button variant="outline-primary" className="w-100">Ver Detalhes</Button>
       </Card.Body>
     </Card>
   );
