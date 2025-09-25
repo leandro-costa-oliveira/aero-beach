7fdce9cea88713ad6220e9dde49e17f3bc381691
caio_rc <caio.crdc@gmail.com>
2025-09-25 07:40:23 -0300

    refactor: melhorando a responsividade do footer

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -1,16 +1,16 @@
 import logo from "../../assets/img/aero-logo.png";
-import { Col, Container, Nav, Row } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import '../../index.css'
 
 export function Footer() {
   return (
     <footer className={`bg-dark container-fluid mt-auto d-flex py-4 px-5`}>
       <Container fluid className="border-top pt-3">
-        <Row className="m-2">
+        <Row>
           <Col>
-            <div className="d-flex flex-row">
-              <img src={logo} alt="AeroBeach" height="44"/>
-              <p className="m-0 px-3 py-2 text-light">© 2025 AltSoft. Todos os direitos reservados.</p>
+            <div className="d-flex flex-column flex-md-row justify-content-center justify-content-md-start">
+              <img src={logo} alt="AeroBeach" style={{height: '2.4rem', width: '3.5rem'}}/>
+              <p className="m-0 px-md-3 py-2 text-light">© 2025 AltSoft. Todos os direitos reservados.</p>
             </div>
           </Col>
         </Row>
