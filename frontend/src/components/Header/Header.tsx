9078d471e24ca4de544e68b48366d5836f7d32cb
caio_rc <caio.crdc@gmail.com>
2025-10-03 16:13:04 -0300

    feat: add user avatar

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -2,15 +2,18 @@ import logo from "../../assets/img/aero-logo.png";
 import { Nav, Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "../../index.css"
+import { Avatar } from "../Avatar/Avatar";
 
 export function Header() {
   return (
-    <Navbar expand="sm" className="bg-dark navbar-dark container-fluid px-2 mb-3">
+    <Navbar expand="sm" className="bg-dark navbar-dark container-fluid px-4 mb-3">
       <Navbar.Brand href="#inicio" style={{ backgroundColor: '#343b41' }} className="gap-1 px-4 rounded-5 d-flex">
         <img src={logo} height="30" />
         AeroBeach
       </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+      <Navbar.Toggle  aria-controls="basic-navbar-nav">
+        <Avatar/>
+      </Navbar.Toggle>
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="gap-3">
           <Link to={'/'} className="lightLink align-content-center">Inicio</Link>
@@ -19,6 +22,7 @@ export function Header() {
           <Link to={'/'} className="lightLink align-content-center">Sobre</Link>
         </Nav>
       </Navbar.Collapse>
+      <Avatar className="avatarDesktop"/>
     </Navbar>
   );
 }
