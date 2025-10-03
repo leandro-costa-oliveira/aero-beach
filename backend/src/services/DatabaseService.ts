06d49c81d32814228e3c01021b37091fa5f8eb0b
caio_rc <caio.crdc@gmail.com>
2025-10-03 10:50:25 -0300

    better SQLite removed

--- backend/src/services/DatabaseService.ts
+++ backend/src/services/DatabaseService.ts
@@ -1,10 +1,3 @@
-import { PrismaBetterSQLite3 } from "@prisma/adapter-better-sqlite3";
 import { PrismaClient } from "../../generated/prisma";
 
-
-
-
-const adapter = new PrismaBetterSQLite3({
-  url: "file:./prisma/aerobeach.db",
-});
 export const prisma = new PrismaClient();
