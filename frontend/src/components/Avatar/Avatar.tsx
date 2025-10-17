c83e47e464b6dcd934c6287e786221e50dfd0172
caio_rc <caio.crdc@gmail.com>
2025-10-17 08:20:53 -0300

    refactor: remove UseState from avatar component

--- frontend/src/components/Avatar/Avatar.tsx
+++ frontend/src/components/Avatar/Avatar.tsx
@@ -1,12 +1,8 @@
 import { Image } from "react-bootstrap";
 import defaltAvatar from "../../assets/img/Avatar.jpeg"
-import { useState } from "react";
 
 export function Avatar (props: React.HTMLAttributes<HTMLImageElement>){
-    const [avatar, _] = useState<Base64URLString>(defaltAvatar)
-
-
     return(
-        <Image {...props} src={avatar} roundedCircle height={36}/>
+        <Image {...props} src={defaltAvatar} roundedCircle height={36}/>
     );
 };
