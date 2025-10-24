f6f7a24e9b338da03375fd6d340033ff83b1695d
Leandro Costa <contato@leandrocosta.pro.br>
2025-10-24 09:27:10 -0300

    fix: typos

--- backend/src/services/TournamentService.ts
+++ backend/src/services/TournamentService.ts
@@ -1,14 +1,11 @@
-import { Service } from 'typedi';
+import { BadRequestError } from "routing-controllers";
+import { Service } from "typedi";
+import { TorneioForm } from "../DTOs/TorneioForm";
 import DatabaseService, { prisma } from "./DatabaseService";
-import { BadRequestError } from 'routing-controllers';
-import { TorneioForm } from '../DTOs/TorneioForm';
 
 @Service()
 export class TournamentService {
-
-  constructor(
-    private databaseService: DatabaseService
-  ) {}
+  constructor(private databaseService: DatabaseService) {}
 
   async getAll(page: number = 1, perPage: number = 10) {
     const skip = (page - 1) * perPage;
@@ -39,15 +36,14 @@ export class TournamentService {
     });
   }
 
-  async createTornament(tournament: TorneioForm): Promise<string> {
-  
+  async createTournament(tournament: TorneioForm): Promise<string> {
     if (tournament.dataInicio > tournament.dataRealizacao!) {
       throw new BadRequestError("Data de início não pode ser maior que a data de realização do torneio.");
     }
     if (tournament.dataLimiteInscricao > tournament.dataRealizacao!) {
       throw new BadRequestError("Data limite de inscrição não pode ser maior que a data de realização do torneio.");
     }
-    if (tournament.dataLimiteInscricao < tournament.dataInicio) {    
+    if (tournament.dataLimiteInscricao < tournament.dataInicio) {
       throw new BadRequestError("Data limite de inscrição não pode ser menor que a data de início do torneio.");
     }
 
