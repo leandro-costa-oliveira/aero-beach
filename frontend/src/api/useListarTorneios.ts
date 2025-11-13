1ced590f772b89ee99a436e6c6474f1147ec4d33
caio_rc <caio.crdc@gmail.com>
2025-11-13 13:34:21 -0300

    comantando queryClient

--- frontend/src/api/useListarTorneios.ts
+++ frontend/src/api/useListarTorneios.ts
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import { apiClient } from "./api-client";
 import { QueryKeys } from "./queryKeys";
 import type { Torneio } from "./torneio.t";
-import { queryClient } from "./queryClient";
+//import { queryClient } from "./queryClient";
 
 interface ListaTorneios<T> {
   data: T[];
@@ -18,10 +18,10 @@ export function useListarTorneios(page: number = 1, perPage: number = 6) {
     queryFn: async () => {
       const resp = await apiClient.get<ListaTorneios<Torneio>>(`/torneios/?page=${page}&perPage=${perPage}`);
 
-      resp.data.data.forEach((torneio) => {
-        // queryClient.setQueryData(QueryKeys.useTorneio(torneio.id), torneio);
-      });
-      return resp.data;
+     // resp.data.data.forEach((torneio) => {
+     //   queryClient.setQueryData(QueryKeys.useTorneio(torneio.id), torneio);
+     // });
+     return resp.data;
     },
   });
 }
