b03863b7dcef15bae082224df552b64e78d9bad8
Leandro Costa <contato@leandrocosta.pro.br>
2025-12-01 16:28:05 -0300

    fix: queryKey

--- frontend/src/hooks/useTorneioById.ts
+++ frontend/src/hooks/useTorneioById.ts
@@ -1,15 +1,13 @@
 import { useQuery } from "@tanstack/react-query";
-import { QueryKeys } from "../api/queryKeys";
 import { apiClient } from "../api/api-client";
+import { QueryKeys } from "../api/queryKeys";
 import type { Torneio } from "../api/torneio.t";
 
 export function useTorneioById(id: string) {
   return useQuery({
-    queryKey: [QueryKeys.TorneioById, id],
+    queryKey: [QueryKeys.useTorneio, id],
     queryFn: async () => {
-      const torneio = await apiClient
-        .get<Torneio>(`/torneios/${id}`)
-        .then((resp) => resp.data);
+      const torneio = await apiClient.get<Torneio>(`/torneios/${id}`).then((resp) => resp.data);
       return torneio;
     },
   });
