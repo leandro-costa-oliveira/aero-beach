d7fe2d659f622d790043f77166a0a6f5be7a3460
caio_rc <caio.crdc@gmail.com>
2025-12-02 09:39:00 -0300

    fix: adapting factory to new model torneios

--- backend/tests/Factories.ts
+++ backend/tests/Factories.ts
@@ -9,13 +9,8 @@ import { TorneioInscricaoForm } from "../src/DTOs/TorneioInscricaoForm";
 export const tournamentFormFactory = Factory.define<TorneioForm>(() => ({
   nome: faker.person.firstName(),
   dataInicio: new Date("2023-10-10"),
-  dataRealizacao: new Date("2023-10-20"),
   dataLimiteInscricao: new Date("2023-10-15"),
-  categoria: faker.helpers.arrayElement(Object.values(CategoriaTorneio)),
-  modalidade: faker.helpers.arrayElement(Object.values(ModalidadeTorneio)),
-  tipo: faker.helpers.arrayElement(Object.values(TipoTorneio)),
   federado: faker.datatype.boolean(),
-  valorInscricao: faker.number.float({ min: 10, max: 50, fractionDigits: 2 }),
   situacao: "aberto",
 }));
 
