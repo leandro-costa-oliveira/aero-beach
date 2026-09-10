08869b92f142dbca7cd8951b3b1c8e688a3b19dc
Davi <marianodavi554@gmail.com>
2026-09-10 16:02:59 -0300

    feat: fixed the frontend errors

--- frontend/src/Context/AuthContext.tsx
+++ frontend/src/Context/AuthContext.tsx
@@ -1,9 +1,36 @@
-import { createContext } from "react";
+import {
+  createContext,
+  useState,
+  type Dispatch,
+  type SetStateAction,
+  type ReactNode,
+} from "react";
 
-export const AuthContext = createContext<{
+interface AuthContextType {
   accessToken: string | null;
-  setAccessToken: React.Dispatch<React.SetStateAction<string | null>>;
-}>({
+  setAccessToken: Dispatch<SetStateAction<string | null>>;
+}
+
+export const AuthContext = createContext<AuthContextType>({
   accessToken: null,
   setAccessToken: () => {},
 });
+
+interface AuthProviderProps {
+  children: ReactNode;
+}
+
+export const AuthProvider = ({ children }: AuthProviderProps) => {
+  const [accessToken, setAccessToken] = useState<string | null>(null);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        accessToken,
+        setAccessToken,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
--- frontend/src/api/useListarTorneios.ts
+++ frontend/src/api/useListarTorneios.ts
@@ -1,25 +1,14 @@
 import { useQuery } from "@tanstack/react-query";
-import { apiClient } from "./api-client";
-import { queryClient } from "./queryClient";
-import { QueryKeys } from "./queryKeys";
-import type { Torneio, ListaTorneios } from "./torneio.t";
+import { apiClient } from "../../../frontend/src/api/api-client";
+import type { Torneio } from "../../../api-schema/TorneioDTO";
 
-export function useListarTorneios(page: number = 1, perPage: number = 6) {
-  return useQuery({
-    queryKey: [...QueryKeys.ListarTorneios, page, perPage],
+export function useListarTorneios() {
+  return useQuery<Torneio[]>({
+    queryKey: ["torneios"],
     queryFn: async () => {
-      const resp = await apiClient.get<ListaTorneios<Torneio>>(
-        `/torneios/?page=${page}&perPage=${perPage}`
-      );
+      const response = await apiClient.get("/torneios");
 
-      resp.data.data.forEach((torneio) => {
-        queryClient.setQueryData(
-          QueryKeys.useTorneio(torneio.id),
-          torneio
-        );
-      });
-
-      return resp.data;
+      return response.data;
     },
   });
 }
